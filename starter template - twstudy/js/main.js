e6a56fc56d4e6188fc1fb100234a4270c40db6e4
nidauljannah114 <nidajannah114@gmail.com>
2022-11-01 01:45:24 +0700

    update

---
+++ starter template - twstudy/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+/*-----------------------
+testimonial slider
+--------------------------*/
+
+function testimonialSlider(){
+    const carouselOne = document.getElementById('carouselOne');
+    if(carouselOne){
+        carouselOne.addEventListener('slide.bs.carousel', function () {
+            const activeItem = this.querySelector(".active");
+            document.querySelector(".js-testimonial-img").src =
+            activeItem.getAttribute("data-js-testimonial-img");
+          })
+    }
+}
+testimonialSlider();
